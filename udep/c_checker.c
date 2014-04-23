74adb90cec391f25f092521e21c9b919c7568cd9
Jonathan Webb <jwebbed@bell.net>
2014-04-23 17:44:54 -0400

    Added msvs support

--- udep/c_checker.c
+++ udep/c_checker.c
@@ -6,6 +6,7 @@
 #include "sets.h"
 #include "c_checker.h"
 
+
 enum include {
     pound,
     i,
--- udep/sets.c
+++ udep/sets.c
@@ -4,7 +4,6 @@
 
 #include "sets.h"
 
-
 struct set* initSet(){
     struct set* set = malloc(sizeof(struct set));
     set->head = NULL;
