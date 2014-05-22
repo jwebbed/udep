79ee65ce0f2dde22af58ceb2ad85a25f58c64334
Jonathan Webb <jwebbed@outlook.com>
2014-05-22 19:56:43 -0400

    :(

---
+++ c_map.h
@@ -0,0 +1,18 @@
+//
+//  c_map.h
+//  udep
+//
+//  Created by Jonathan Webb on 2014-05-22.
+//  Copyright (c) 2014 Jonathan Webb. All rights reserved.
+//
+
+#ifndef udep_c_map_h
+#define udep_c_map_h
+
+#include <map>
+#include <string>
+#include "c_set.h"
+
+#endif
+
+typedef std::map<std::string, c_set> c_map_t;
